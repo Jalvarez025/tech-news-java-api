1433d5e59dd7f977733bde8f546a51821e95bfbd
Jesse Alvarez <Jalvarez025@yahoo.com>
2022-08-06 19:59:52 -0500

    Added entity models

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,14 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int oneInt = 4;
+        double oneDouble = 6.9;
+        String oneString = " One world";
+
+        double sum = oneInt + oneDouble;
+        String concat = oneInt + oneString;
+        System.out.println(sum);
+        System.out.println(concat);
+    }
+}
